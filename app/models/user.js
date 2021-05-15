5ad10b873c0f0b5030acedfcf235cc9780d2486f
LucasCtrl <lucasalt@protonmail.com>
2021-05-15 16:41:18 +0200

    :recycle: Full MongoDB integration

--- app/models/user.js
+++ app/models/user.js
@@ -1,48 +1,13 @@
-const { firebase, db } = require('../db')
+const mongoose = require('mongoose')
 
-// Create an user
-const create = async (userId, guildId) => {
-  const data = {
-    level: 0,
-    messages: 0,
-    experience: 0,
-    userId: userId,
-    guildId: guildId,
-  }
+const UserSchema = new mongoose.Schema({
+  experience: { type: Number, required: true },
+  guildId: { type: String, required: true },
+  level: { type: Number, required: true },
+  messages: { type: Number, required: true },
+  userId: { type: String, required: true },
+})
 
-  const res = await db.collection('users').add(data)
-  return res
-}
+const model = mongoose.model('users', UserSchema)
 
-// Search an user
-const search = async (userId, guildId) => {
-  const res = await db.collection('users').where('userId', '==', userId).where('guildId', '==', guildId).get()
-  return res
-}
-
-// Get an user
-const get = async (id) => {
-  const res = await db.collection('users').doc(id).get()
-  return res
-}
-
-// Update an user
-
-/**
- * Update an user
- * @param {String} id - User id
- * @param {Number} experience - Experience gained
- * @param {Number} level - Level gained
- */
-const update = async (id, experience, level) => {
-  await db
-    .collection('users')
-    .doc(id)
-    .update({
-      messages: firebase.firestore.FieldValue.increment(1),
-      experience: firebase.firestore.FieldValue.increment(experience),
-      level: firebase.firestore.FieldValue.increment(level),
-    })
-}
-
-module.exports = { create, search, get, update }
+module.exports = model
